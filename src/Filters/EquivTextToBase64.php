c9f3eaf694a4e1fec28c9e566c8b09f9457c4d62
Mauro Cassani <maurocassani1978@gmail.com>
2025-08-05 12:14:08 +0200

    Encoding equiv-text attributes

---
+++ src/Filters/EquivTextToBase64.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author domenico domenico@translated.net / ostico@gmail.com
+ * Date: 13/05/19
+ * Time: 19.37
+ *
+ */
+
+namespace Matecat\SubFiltering\Filters;
+
+
+use Matecat\SubFiltering\Commons\AbstractHandler;
+
+class EquivTextToBase64 extends AbstractHandler {
+
+    public function transform( $segment ) {
+
+        // extract equiv-text attribute
+        preg_match_all('/equiv-text=\"(.*?)\"/', $segment, $equiv_tags);
+
+        if(!empty($equiv_tags[0])){
+            foreach ($equiv_tags[0] as $index => $equiv_tag){
+                $tag = $equiv_tags[1][$index];
+
+                if (strpos($tag, "base64:") === false) {
+                    $b = base64_encode($tag);
+                    $segment = str_replace($equiv_tags[$index], 'equiv-text="base64:'.$b.'"', $segment);
+                }
+            }
+        }
+
+        return $segment;
+
+    }
+
+}
