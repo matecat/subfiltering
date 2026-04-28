85bf1605302eddaa850478186d9dd13688d25a3b
domenico <domenico@translated.net>
2026-04-28 17:18:38 +0200

    ♻️ refactor(filter): replace PercentDoubleCurlyBrackets with MarkupToPh handler in MyMemoryFilter
    - remove unused PercentDoubleCurlyBrackets import
    - update filter pipeline logic to add SmartCounts handler after MarkupToPh instead of PercentDoubleCurlyBrackets

--- src/MyMemoryFilter.php
+++ src/MyMemoryFilter.php
@@ -4,7 +4,7 @@ namespace Matecat\SubFiltering;
 
 use Matecat\SubFiltering\Commons\Pipeline;
 use Matecat\SubFiltering\Filters\DollarCurlyBrackets;
-use Matecat\SubFiltering\Filters\PercentDoubleCurlyBrackets;
+use Matecat\SubFiltering\Filters\MarkupToPh;
 use Matecat\SubFiltering\Filters\SingleCurlyBracketsToPh;
 use Matecat\SubFiltering\Filters\SmartCounts;
 use Matecat\SubFiltering\Filters\TwigToPh;
@@ -70,7 +70,7 @@ class MyMemoryFilter extends AbstractFilter
                 // For Airbnb, add the SmartCounts handler to process specific variable syntax
                 // that looks like `%{smart_count}`.
                 if (!$channel->contains(SmartCounts::class)) {
-                    $channel->addAfter(PercentDoubleCurlyBrackets::class, SmartCounts::class);
+                    $channel->addAfter(MarkupToPh::class, SmartCounts::class);
                 }
                 break;
 
